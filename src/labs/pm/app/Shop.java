a1b0f7bfdc5eec50004243b33e3110d84c914af3
Nidio Dolfini <nidiosdolfini@gmail.com>
2021-03-05 17:36:33 -0300

    first commit

---
+++ src/labs/pm/app/Shop.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package labs.pm.app;
+
+import java.math.BigDecimal;
+import labs.pm.data.Product;
+
+/**
+ *
+ * @author nidio
+ */
+public class Shop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Product p1 = new Product();
+        p1.setId(101);
+        p1.setName("Tea");
+        p1.setPrice(BigDecimal.valueOf(1.99));
+        
+        System.out.println(p1.getId() + " " + p1.getName() + " " + p1.getPrice() + " " + p1.getDiscount());
+    }
+    
+}
---
+++ src/labs/pm/data/Product.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2021 nidio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package labs.pm.data;
+
+import java.math.BigDecimal;
+import static java.math.RoundingMode.HALF_UP;
+
+/**
+ *
+ * @author nidio
+ */
+public class Product {
+    
+    public static final BigDecimal DISCOUNT_RATE = BigDecimal.valueOf(0.1);
+    private int id;
+    private String name;
+    private BigDecimal price;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(final int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(final BigDecimal price) {
+        this.price = price;
+    }
+    
+    public BigDecimal getDiscount(){
+        return price.multiply(DISCOUNT_RATE).setScale(2,HALF_UP);
+    }
+    
+}
